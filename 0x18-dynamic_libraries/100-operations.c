c1c4619f49cddcf628ccefa7016cef1c30ce0ed3
ezzaytouni othmane <ezzaytouniothmane@gmail.com>
2024-01-15 05:53:55 -0800

    dinamic_lib

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - addition
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int add(int a, int b)
+{
+      return (a + b);
+}
+/**
+ * sub - sub
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int sub(int a, int b)
+{
+      return (a - b);
+}
+/**
+ * mul - mult
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int mul(int a, int b)
+{
+      return (a * b);
+}
+/**
+ * div - divi
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int div(int a, int b)
+{
+      return (a / b);
+}
+/**
+ * mod - mod
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int mod(int a, int b)
+{
+      return (a % b);
+}
